d33ae1da7494d8182eeea38b6645ccefcdbbc544
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-11-16 00:32:00 +0800

    config base

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,36 +1,24 @@
+window._ = require('lodash');
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+try {
+    window.$ = window.jQuery = require('jquery');
 
-require('./bootstrap');
+    require('uikit');
+} catch (e) {}
 
-window.Vue = require('vue');
+window.axios = require('axios');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+let token = document.head.querySelector('meta[name="csrf-token"]');
 
-// const files = require.context('./', true, /\.vue$/i)
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
-// files.keys().map(key => {
-//     return Vue.component(_.last(key.split('/')).split('.')[0], files(key))
-// })
+// custom js
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app'
-});
+require('uikit');
+require('./custom/custom');
