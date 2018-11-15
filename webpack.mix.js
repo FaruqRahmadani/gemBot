348ccdeef1791373d272b53ba002f3f3ae0a008e
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-11-16 00:11:05 +0800

    Bismillah

---
+++ webpack.mix.js
@@ -0,0 +1,13 @@
+let mix = require('laravel-mix');
+
+mix
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .browserSync({
+     proxy:'localhost:8000',
+     scrollRestoreTechnique:'cookie',
+     files: [
+       'resources/**'
+     ],
+     scrollElements: ['.scroller']
+   });
